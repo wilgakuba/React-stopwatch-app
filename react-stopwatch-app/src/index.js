1002819c2f10afb3687e4eded3b6a1fd122d75b3
Jakub Wilga <wilga327@wp.pl>
2023-04-21 20:48:03 +0200

    Add components and finish

--- react-stopwatch-app/src/index.js
+++ react-stopwatch-app/src/index.js
@@ -1,9 +1,11 @@
+import './styles/normalize.scss';
+import './styles/global.scss';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
 );
